08dff3ad71ccb0bcabaa2da81522787bbffd6d93
alappie <ashley@specialtyproduce.com>
2021-09-13 12:17:32 -0700

    completed assessment

---
+++ assesment-week-1-coding-concepts.js
@@ -0,0 +1,58 @@
+// ASSESSMENT 1: Coding conceptual questions
+
+// Examine the following examples.
+
+// First, predict the outcome based on your understanding of the code.
+// Then, uncomment the console.log() and verify the output. Briefly explain why your initial answer was correct or incorrect.
+
+// --------------------1) What will this log?
+//
+// var cohort = "Delta 2021"
+// console.log(cohort.length)
+//
+// a) Your answer: 10
+// b) Verify and explain:This will log the length of "delta 2021"
+
+
+// --------------------2) What will this log?
+//
+// var greeting = "Hello World!"
+// console.log(greeting[3])
+
+
+// a) Your answer:This Should log the location of the 3rd letter in the greeting which is "l" the second "l" in specific.
+
+// var greeting = "Hello World!"
+// console.log("greeting"[3]);
+
+// b) Verify and explain: Ok, It came back as "e" because we are logging "greeting" not "hellow world"
+
+
+// --------------------3) What will this log?
+
+// var languages = ["JavaScript", "Ruby", "Python", "C++"]
+// var index = 1
+// console.log(languages[index])
+
+// a) Your answer: Ruby
+// b) Verify and explain: because it is Counting JavaScript as the zeroth space and ruby would be index 1.
+
+
+// --------------------4) What will this log?
+
+// var weekendDays = ["saturday", "sunday"]
+// console.log(weekendDays.toUpperCase())
+
+// a) Your answer: It should show "SATURDAY" AND "SUNDAY"
+// b) Verify and explain: It is showing that .toUpperCase() is not a function when I run it. Either the upper case comand is not a recognised function in JavaScript or 'saturday' and 'sunday' are still in string form.
+
+
+
+// --------------------5) What will this log?
+//
+// var dataTypes = ["number", "string", "Boolean"]
+// console.log(typeof dataTypes.length);
+
+
+// a) Your answer: This will Log the length of dataTypes array and return the number 3 because that is the number of variables in the array.
+// b) Verify and explain: it returned "number" because the comand is asking for the data type and the data type is number.
--- coding-concepts.js
+++ coding-concepts.js
@@ -6,17 +6,17 @@
 // Then, uncomment the console.log() and verify the output. Briefly explain why your initial answer was correct or incorrect.
 
 // --------------------1) What will this log?
-
-var cohort = "Delta 2021"
+//
+// var cohort = "Delta 2021"
 // console.log(cohort.length)
-
-// a) Your answer:
-// b) Verify and explain:
+//
+// a) Your answer: 10
+// b) Verify and explain:This will log the length of "delta 2021"
 
 
 // --------------------2) What will this log?
 
-var greeting = "Hello World!"
+// var greeting = "Hello World!"
 // console.log(greeting[3])
 
 // a) Your answer:
@@ -25,8 +25,8 @@ var greeting = "Hello World!"
 
 // --------------------3) What will this log?
 
-var languages = ["JavaScript", "Ruby", "Python", "C++"]
-var index = 1
+// var languages = ["JavaScript", "Ruby", "Python", "C++"]
+// var index = 1
 // console.log(languages[index])
 
 // a) Your answer:
@@ -35,7 +35,7 @@ var index = 1
 
 // --------------------4) What will this log?
 
-var weekendDays = ["saturday", "sunday"]
+// var weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
 // a) Your answer:
@@ -43,9 +43,11 @@ var weekendDays = ["saturday", "sunday"]
 
 
 // --------------------5) What will this log?
-
-var dataTypes = ["number", "string", "Boolean"]
-// console.log(typeof dataTypes.length)
+//
+// This will Log the length of dataTypes array and return the number 3m because that is the number of variables in the array.
+// //
+// var dataTypes = ["number", "string", "Boolean"]
+÷
 
 // a) Your answer:
 // b) Verify and explain:
